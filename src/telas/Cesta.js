280404ac2a32ce102aea50f85de6dacac3092c33
leonilso <wrublak.leonilso@escola.pr.gov.br>
2026-04-16 11:39:15 -0300

    adicionanto textos

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,12 +1,19 @@
-import { Image, StyleSheet, Dimensions, Text} from "react-native";
+import { Image, StyleSheet, Dimensions, Text, View} from "react-native";
 import topo from "../../assets/topo.png";
-
+``
 const width = Dimensions.get("screen").width
 
 export default function Cesta(){
     return <>
         <Image style={estilos.topo} source={topo}/>
+
         <Text style={estilos.titulo}>Detalhes da cesta</Text>
+        <View style={estilos.cesta}>
+            <Text style={estilos.nome}>Cesta de verduras</Text>
+            <Text>Fazenda do Leôncio</Text>
+            <Text>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Text>
+            <Text>R$ 40, 00</Text>
+        </View>
     </>
     
 }
@@ -25,5 +32,15 @@ const estilos = StyleSheet.create({
         lineHeight: 26,
         padding: 16,
         fontWeight: "bold"
+    }, 
+    nome: {
+        color: "#464646",
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: "bold"
+    },
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16,
     }
 });
